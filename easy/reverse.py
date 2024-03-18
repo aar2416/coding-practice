17493b098c0b49bf41f9c2f61bdc5298557df3c8
Aaroosh Pandoh <aar2416@gmail.com>
2024-03-18 08:12:57 -0500

    function name change

--- easy/reverse.py
+++ easy/reverse.py
@@ -1,8 +1,8 @@
-def recursive(string, index, final_string):
+def reverse_string(string, index, final_string):
     final_string += string[len(string)-1-index]
     if index == len(string)-1:
         return final_string
-    return recursive(string, index+1, final_string)
+    return reverse_string(string, index+1, final_string)
 
-final_string = recursive("string", 0, "")
+final_string = reverse_string("string", 0, "")
 print(final_string)
